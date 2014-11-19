a53b1c4713315ebf97abced9bc35ae9482a8893d
Viktor Vaughn <sbsteven@andrew.cmu.edu>
2014-11-18 19:53:38 -0500

    Added commandline flag for num_sites
    Added a flag to pick how many sites to grab from Alexa.

--- get_top_sites/nTopSitesByCountry.py
+++ get_top_sites/nTopSitesByCountry.py
@@ -3,7 +3,7 @@ import experiment.alexa as alexa
 
 def usage():
     print "Not enough arguments! See below:"
-    print "python nTopSitesByCountry.py <2_letter_country_code> <output_file>"
+    print "python nTopSitesByCountry.py <2_letter_country_code> <num_sites> <output_file>"
 
 def collect_sites_from_alexa(alexa_link="http://www.alexa.com/topsites", 
 		output_file="out.txt", nsites=5, browser="firefox"):
@@ -22,12 +22,17 @@ def collect_sites_from_alexa(alexa_link="http://www.alexa.com/topsites",
 	alexa.run_script(alexa_link, output_file, nsites, browser)
 	print "Collection Complete. Results stored in ", output_file
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     usage()
     sys.exit()
 
+#2-letter country code
 country = sys.argv[1]
-out_file = sys.argv[2]
+#Number of sites to grab
+n = sys.argv[2]
+#Output file -- will be created if it doesn't exist
+out_file = sys.argv[3]
 
-collect_sites_from_alexa(nsites=250, output_file=out_file, browser="firefox", 
+
+collect_sites_from_alexa(nsites=int(n), output_file=out_file, browser="firefox", 
             alexa_link="http://www.alexa.com/topsites/countries/" + country)
